47c94a1c23cd556078ffcc77445b1784f0c33b0b
n0va-scy <1062130080@qq.com>
2020-06-01 20:20:53 +0800

    更新bash.sh

--- bash.sh
+++ bash.sh
@@ -6,8 +6,8 @@ fi
 
 temp=`sed -n '1p' Makefile`
 echo $temp
-if [ "$temp" != "CC = afl-gcc" ];then
+if [ "$temp" != "CC = /out/afl-gcc" ];then
 	echo "run this"
-	sed -i '1i\CC = afl-gcc' Makefile
+	sed -i '1i\CC = /out/afl-gcc' Makefile
 fi
 make
